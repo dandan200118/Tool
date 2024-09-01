13dd6d9fcf81e372bfc1b7e85e473f47b7f415ad
nai-degen <khoners@protonmail.com>
2024-09-01 17:06:09 -0500

    fixes comment typos

--- src/shared/key-management/prioritize-keys.ts
+++ src/shared/key-management/prioritize-keys.ts
@@ -10,20 +10,13 @@ import { Key } from "./index";
  *    b. Otherwise, select the first key.
  * 2. Keys which have not been used in the longest time
  * 3. Keys according to the custom comparator, if provided
- * 4. Keys which have been used the longest time
- * @param keys
- * @param customComparator
+ * @param keys The list of keys to sort
+ * @param customComparator A custom comparator function to use for sorting
  */
 export function prioritizeKeys<T extends Key>(
   keys: T[],
   customComparator?: (a: T, b: T) => number
 ) {
-  // Sorts keys from highest priority to lowest priority, where priority is:
-  // 1. Keys which are not rate limited
-  //    a. If all keys were rate limited recently, select the least-recently
-  //       rate limited key.
-  // 2. Keys which have not been used in the longest time
-
   const now = Date.now();
 
   return keys.sort((a, b) => {
